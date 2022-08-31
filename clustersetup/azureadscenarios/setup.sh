1887aa0ebf2cb704454cc29682d8a96c14504f05
Ricardo Machado <rjfmachado@users.noreply.github.com>
2022-08-31 12:20:08 +0000

    updates

--- clustersetup/azureadscenarios/setup.sh
+++ clustersetup/azureadscenarios/setup.sh
@@ -8,12 +8,13 @@ export USER_DEVELOPER_ID=$(az ad user list --upn $USER_DEVELOPER_UPN -o tsv --qu
 
 kubectl create namespace testdeveloper
 kubectl run my-nginx --image=nginx --port=80 --namespace testdeveloper
+kubectl apply -f clustersetup/azureadscenarios/testdeployment.yaml
 
 export AKS_ID=$(az aks show -g cloudnative -n cloudnative --query id -o tsv)
 
 # get access to kubeconfig
 az role assignment create --role "Azure Kubernetes Service Cluster User Role" --assignee $USER_DEVELOPER_ID --scope "$AKS_ID"
-# get read access to cluster objects (makes portal happy, kubectl does not need this)
+# get read access to cluster objects (makes portal,vscode extension happy, kubectl does not need this)
 az role assignment create --role "Azure Kubernetes Service RBAC Reader" --assignee $USER_DEVELOPER_ID --scope "$AKS_ID"
 # get write access to namespace
 az role assignment create --role "Azure Kubernetes Service RBAC Admin" --assignee $USER_DEVELOPER_ID --scope "$AKS_ID/namespaces/testdeveloper"
@@ -22,5 +23,5 @@ az role assignment create --role "Azure Kubernetes Service RBAC Admin" --assigne
 az role definition list | grep Kubernetes
 az role definition list -n "Azure Kubernetes Service RBAC Reader" -o jsonc
 
-# wee bit of powershell
+# wee bit of powershell to clean up role assignments
 Get-AzRoleAssignment | where {$_.SignInName -eq 'developer@ricardomachado.net'} | Remove-AzRoleAssignment
